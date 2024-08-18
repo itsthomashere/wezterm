ab38277887b34fefbfeaa4642cf2f047f48cd2ff
BMHDAC <bdachai308@gmail.com>
2024-08-18 18:42:52 +0700

    Update font rendering

--- wezterm.lua
+++ wezterm.lua
@@ -3,13 +3,15 @@ local wezterm = require("wezterm")
 local config = {}
 config.max_fps = 240
 config.colors = require("color")
+config.freetype_load_target = "Normal"
+config.freetype_render_target = "HorizontalLcd"
 config.window_background_opacity = 0.8
 config.font_shaper = "Harfbuzz"
 config.font = wezterm.font({
-	family = "VictorMono NF",
+	family = "MapleMono NF",
 })
 
-config.font_size = 21
+config.font_size = 20.5
 config.window_padding = {
 	left = 0,
 	right = 0,
